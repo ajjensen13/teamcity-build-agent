c9a3460e0796499d40fe88e86a4668ce27606884
A. Jensen <jensen.aaro@gmail.com>
2020-05-12 14:31:10 -0500

    fix

--- build/run-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm /var/run/docker.pid 2>/dev/null
-service docker start
-echo "Docker daemon started"
---
+++ build/setup-config.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+mkdir -p "/data/teamcity_agent/conf"
+PROPERTIES_FILE="/data/teamcity_agent/conf/buildAgent.properties"
+
+command -v dart > /dev/null && \
+  printf "dart.path=%s\n" "$(command -v dart)" >> "$PROPERTIES_FILE" && \
+  printf "dart.version=%s\n" "$(dart --version 2>&1)" >> "$PROPERTIES_FILE"
+
+command -v pub > /dev/null && \
+  printf "pub.path=%s\n" "$(command -v pub)" >> "$PROPERTIES_FILE" && \
+  printf "pub.version=%s\n" "$(pub version)" >> "$PROPERTIES_FILE"
+
+command -v webdev > /dev/null && \
+  printf "webdev.path=%s\n" "$(command -v webdev)" >> "$PROPERTIES_FILE" && \
+  printf "webdev.version=%s\n" "$(webdev --version)" >> "$PROPERTIES_FILE"
+
+command -v node > /dev/null && \
+  printf "node.path=%s\n" "$(command -v node)" >> "$PROPERTIES_FILE" && \
+  printf "node.version=%s\n" "$(node --version)" >> "$PROPERTIES_FILE"
+
+command -v npm > /dev/null && \
+  printf "npm.path=%s\n" "$(command -v npm)" >> "$PROPERTIES_FILE" && \
+  printf "npm.version=%s\n" "$(npm --version)" >> "$PROPERTIES_FILE"
+
+command -v go > /dev/null && \
+  printf "go.path=%s\n" "$(command -v go)" >> "$PROPERTIES_FILE" && \
+  printf "go.version=%s\n" "$(go version)" >> "$PROPERTIES_FILE"
+
